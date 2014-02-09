0b0270a61e50b0dcdd09f5496c7a3dd10665da67
U-X3\lxgreen <lxgreen@X3.(none)>
2014-02-10 01:20:37 +0200

    port 5000 used now

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.configure(function () {
         express.static(__dirname) //where your static content is located in your filesystem
     );
 });
-app.listen(80); //the port you want to use
+app.listen(5000); //the port you want to use
