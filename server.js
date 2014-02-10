e0b1a9972878734a4734ea047da10a1d94a3b68b
U-X3\lxgreen <lxgreen@X3.(none)>
2014-02-10 21:37:59 +0200

    gzip compression added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.configure(function () {
+    app.use(express.compress());
     app.use(
         "/", //the URL throught which you want to access to you static content
         express.static(__dirname) //where your static content is located in your filesystem
